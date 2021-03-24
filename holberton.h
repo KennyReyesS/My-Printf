c1d8ce11cb6343dbd5109a6921d254759ca9e4ab
KennyReyesS <kennyreyes2000@gmail.com>
2021-03-24 00:06:47 +0000

    holberton.h updated

--- holberton.h
+++ holberton.h
@@ -14,8 +14,7 @@ typedef struct data
 } data_t;
 
 int _putchar(char c);
-int (*get_type_func(const char k))(va_list);
-int print_char(va_list l);
+int print_char(va_list c);
 int print_str(va_list s);
 int print_dec(va_list n);
 int print_rev(va_list r);
